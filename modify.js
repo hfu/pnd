24e9d3b835d09c786f71804c0ef27925a08c552e
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-05-30 09:44:58 -0400

    update

---
+++ modify.js
@@ -0,0 +1,3 @@
+module.exports = (f) => {
+  return f
+}
--- pnd.js
+++ pnd.js
@@ -5,25 +5,29 @@ const turf = require('@turf/turf')
 const wkx = require('wkx')
 const fs = require('fs')
 const cpq = require('childprocess-queue')
+const modify = require('./modify.js')
 const data = config.get('data')
 
 cpq.setMaxProcesses(1)
 let pools = {}
 for (let database of Object.keys(data)) {
   pools[database] = new Pool({
-    host: config.get('host'), user: config.get('user'), 
-    password: config.get('password'), database: database, max: 1000
+    host: config.get('host'),
+    user: config.get('user'),
+    password: config.get('password'),
+    database: database,
+    max: 1000
   })
 }
 
 const pnd = async function (module) {
   const stream = fs.createWriteStream(`${module.join('-')}.ndjson`)
   const bbox = tilebelt.tileToBBOX([module[1], module[2], module[0]])
-  let layer_count = 0
+  let layerCount = 0
   for (const database of Object.keys(data)) {
-    layer_count += data[database].length
-    for(const t of data[database]) {
-      const [layer, minzoom, maxzoom, text_key] = t
+    layerCount += data[database].length
+    for (const t of data[database]) {
+      const [layer, minzoom, maxzoom, textKey] = t
       const client = await pools[database].connect()
       const geom = config.get('geom')[database]
       let q = `SELECT * FROM ${layer}`
@@ -32,61 +36,62 @@ const pnd = async function (module) {
         `ST_MakePoint(${bbox[2]}, ${bbox[3]}))`
       await client.query(new Query(q))
         .on('row', row => {
-          let g = wkx.Geometry.parse(new Buffer(row[geom], 'hex')).toGeoJSON()
+          let g = wkx.Geometry.parse(Buffer.from(row[geom], 'hex')).toGeoJSON()
           if (g.type === 'Point' || g.coordinates.length === 0) {
           } else {
             g = turf.bboxClip(g, bbox).geometry
           }
+          delete row[geom]
+          let properties = row
+          properties._layer = layer
+          if (textKey) properties.text = row[textKey]
           let f = {
             type: 'Feature',
             geometry: g,
-            tippecanoe: {layer: g.type.toLowerCase().replace('multi', ''),
-              minzoom: minzoom, maxzoom: maxzoom}
-            //tippecanoe: {layer: layer, minzoom: minzoom, maxzoom: maxzoom}
+            tippecanoe: {
+              layer: g.type.toLowerCase().replace('multi', ''),
+              minzoom: minzoom,
+              maxzoom: maxzoom
+            },
+            properties: properties
           }
-          delete row[geom]
-          // f.properties = row
-          f.properties = {}
-          if (text_key) f.properties.text = row[text_key]
-          f.properties.layer = layer
-          // f.properties = {layer: layer}
-          stream.write(JSON.stringify(f) + '\n')
+          stream.write(JSON.stringify(modify(f)) + '\n')
         })
         .on('end', () => {
-          layer_count -= 1
+          layerCount -= 1
           client.end()
-          if(layer_count === 0) stream.end()
+          if (layerCount === 0) stream.end()
         })
     }
   }
   stream.on('close', () => {
-    cpq.spawn('nice', ['-19', 'tippecanoe', 
+    cpq.spawn('nice', ['-19', 'tippecanoe',
       '--read-parallel',
       '--simplify-only-low-zooms', '--simplification=4', '--minimum-zoom=5',
-      '--maximum-zoom=16', '--base-zoom=16', '-f', 
-      `--output=${module.join('-')}.mbtiles`, `${module.join('-')}.ndjson`], 
-      {
-        stdio: 'inherit',
-        onCreate: (proc) => {
-          proc.on('close', (code) => {
-            console.log(`${proc.spawnargs[11]} finished. ` + 
+      '--maximum-zoom=16', '--base-zoom=16', '-f',
+      `--output=${module.join('-')}.mbtiles`, `${module.join('-')}.ndjson`],
+    {
+      stdio: 'inherit',
+      onCreate: (proc) => {
+        proc.on('close', (code) => {
+          console.log(`${proc.spawnargs[11]} finished. ` +
               `${cpq.getCurrentProcessCount()} active, ` +
               `${cpq.getCurrentQueueSize()} in queue.`)
-          })
-        }
-      })
-    console.log(`pushed a tippecanoe process: ` + 
-      `${cpq.getCurrentProcessCount()} of ${cpq.getMaxProcesses()} ` + 
+        })
+      }
+    })
+    console.log(`pushed a tippecanoe process: ` +
+      `${cpq.getCurrentProcessCount()} of ${cpq.getMaxProcesses()} ` +
       `active, ${cpq.getCurrentQueueSize()} in queue.`)
   })
 }
 
-async function main() {
+async function main () {
   let ct = 0
   for (const module of config.get('modules')) {
     ct += 1
     console.log(`pushing #${ct} ${module.join('-')}`)
-    await pnd(module) 
+    await pnd(module)
   }
 }
 
